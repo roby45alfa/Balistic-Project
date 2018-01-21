c7522b9ac47821a0859322ddfcd3d798d3d76e38
roby45alfa <35657207+roby45alfa@users.noreply.github.com>
2018-01-21 15:39:42 +0100

    Create Balistic Project.cpp

---
+++ Balistic Project.cpp
@@ -0,0 +1,25 @@
+// Balistic Project.cpp : Defines the entry point for the console application.
+//
+#include "stdafx.h"
+#include "Bullet.h"
+#include "FunzioniBPS.h"
+#include <iostream>
+#include <windows.h>
+
+/*
+Scrivere qua eventuali commenti e proposte nel caso ce ne fosse bisogno:
+
+- potremmo usare una enumerazione per identificare la scelta del menu
+- continuare le definizione delle classi
+-
+*/
+
+
+using namespace std;
+
+int main()
+{
+	menu(1);
+	return 0;
+}
+
