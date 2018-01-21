06b9a373bb4594e674fdc5ecf72fd87701d73005
roby45alfa <35657207+roby45alfa@users.noreply.github.com>
2018-01-21 15:41:15 +0100

    Create Bullet.h

---
+++ Bullet.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "Weapon.h"
+
+
+
+class Bullet : protected Weapon {
+private:
+	double size; //dimensione del proiettile
+	double velVolata; //velocità di volata (velocità all'uscita della bocca della canna)
+	double vacdistance; //distanza percorsa dal proiettile nel vuoto
+	double airdistance; //distanza percorsa dal proiettile dell'aria 
+	double vactime; //tempo impiegato dal proiettile per completare la sua traiettoia nel vuoto
+
+protected:
+	double height; //altezza di sparo del proiettile
+public:
+	double VacDistance(Bullet, Weapon); //calcolo distanza massima nel vuoto
+	double VacTime(Bullet, Weapon);
+};
