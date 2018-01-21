5f0b9fb8edcc20212bf1a2f12d76a32d643c2950
Roberto Alfano <alfanoroberto.01@gmail.com>
2018-01-21 15:46:43 +0100

    Revert "Revert "Create FunzioniBPS.h""
    This reverts commit 9d2cfe162f24eceb54bf06f94abfc336f3072d91.

---
+++ FunzioniBPS.h
@@ -0,0 +1,11 @@
+#pragma once
+
+
+#define STDOUT_PAUSE system("PAUSE"); //fa apparire nello schermo "Premi un tasto per continuare..." 
+#define STDOUT_REFRESH system("cls"); //elimina il contenuto di tutto l'output prodotto fino ad quel momento
+
+void setcolor(unsigned short); //funzione setta colori
+
+void menu(int); //funzione gestione menu
+
+
